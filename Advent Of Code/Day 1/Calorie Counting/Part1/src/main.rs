fa1b14677ef38cc6f7565a4ad9631a17c065d8a4
Peter Avgerinos <108369084+PeterAvgerinos@users.noreply.github.com>
2023-07-27 22:33:03 +0300

    aoc day1 part1

---
+++ Advent Of Code/Day 1/Calorie Counting/Part1/src/main.rs
@@ -0,0 +1,58 @@
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+
+fn main() {
+    // file path
+    let file_path = "../data/puzzle_input.txt";
+
+    // Attempt to open the file
+    let file = match File::open(file_path) {
+        Ok(file) => file,
+        Err(error) => {
+            eprintln!("Error opening the file: {}", error);
+            return;
+        }
+    };
+
+    let mut v = Vec::new();
+
+    // Use a BufReader to read the file line by line
+    let reader = BufReader::new(file);
+
+    let mut sum = 0;
+
+    // Process each line in the file
+    for line_result in reader.lines() {
+        match line_result {
+            Ok(line_content) => {
+                if line_content.is_empty() {
+                    v.push(sum);
+                    sum = 0;
+                } else {
+                    match line_content.parse::<i32>() { 
+                        Ok(number) => { 
+                            sum = sum + number;
+                        }
+                        Err(error) => { 
+                            eprintln!("Oopsies {}\n", error);
+                            return;
+                        }
+                    }
+                }
+            }
+            Err(error) => {
+                eprintln!("Error reading line: {}", error);
+            }
+        }
+    }
+
+    let mut global_max = 0;
+
+    for sum in v{
+        if global_max < sum{ 
+            global_max = sum;
+        }
+    }
+
+    println!("{global_max}");
+}
