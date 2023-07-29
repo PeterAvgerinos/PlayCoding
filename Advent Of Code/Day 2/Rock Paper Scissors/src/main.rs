2e96e79b1e6b0b419a221529f68ba449ce47022d
Peter Avgerinos <108369084+PeterAvgerinos@users.noreply.github.com>
2023-07-30 01:08:23 +0300

    day2 part1

--- Advent Of Code/Day 2/Rock Paper Scissors/src/main.rs
+++ Advent Of Code/Day 2/Rock Paper Scissors/src/main.rs
@@ -2,6 +2,48 @@ use std::fs::File;
 use std::io::{BufRead, BufReader};
 use std::env;
 
+struct Turn { 
+    opponent: String,
+    my_move: String,
+}
+
+fn clean_string(mv: String) -> String { 
+    if (mv == "A") || (mv == "X") {return "Rock".to_string();}
+    else if (mv == "B") || (mv == "Y") { return "Paper".to_string();}
+    else if (mv == "C") || (mv == "Z") { return "Scissors".to_string();}
+    else {return "".to_string();}
+}
+
+fn check(turn: Turn) -> i32 { 
+    let rock: String = "Rock".to_string();
+    let paper: String = "paper".to_string();
+    let scissor: String = "scissor".to_string();
+    let mut sum = 0;
+    if turn.my_move == rock { 
+        sum = sum + 1;
+        if turn.opponent == paper {return sum;}
+        else if turn.opponent == scissor {return sum+6;}
+        else if turn.opponent == rock {return sum+3;}
+        return -1;
+    }
+    else if turn.my_move == paper { 
+        sum = sum + 2;
+        if turn.opponent == paper {return sum+3;}
+        else if turn.opponent == scissor {return sum;}
+        else if turn.opponent == rock {return sum+6;}
+        else {return -1;}
+    }
+    else if turn.my_move == scissor { 
+        sum = sum + 3;
+        if turn.opponent == paper {return sum+6;}
+        else if turn.opponent == scissor {return sum+3;}
+        else if turn.opponent == rock {return sum;}
+        else {return -1;}
+    }
+    else {return -1;}
+
+}
+
 fn main() {
 
     let args: Vec<String> = env::args().collect();
@@ -19,6 +61,8 @@ fn main() {
         }
     };
 
+    let mut sum = 0;
+
     let reader = BufReader::new(file);
 
     for line_result in reader.lines()  { 
@@ -28,11 +72,13 @@ fn main() {
                     continue;
                 }
                 else { 
-                    let mut p = Vec::new();
-                    for item in line_content.split_whitespace() { 
-                        p.push(item);
-                    }
-                    println!("{}, {}", p[0], p[1]);
+                    let splitted_string: Vec<&str> = line_content.split(" ").collect();
+                    let turn = Turn { 
+                        opponent: clean_string(splitted_string[0].to_string()),
+                        my_move: clean_string(splitted_string[1].to_string()),
+                    };
+                                
+                    sum = sum + check(turn);
                 }
             }
             Err(_error) => { 
@@ -41,6 +87,7 @@ fn main() {
 
         }
 
-
     }
+
+    println!("{}", sum);
 }
