4831aad59692d9c985980dc5cc810fedb8639a60
Peter Avgerinos <108369084+PeterAvgerinos@users.noreply.github.com>
2023-08-11 23:32:00 +0300

    playing around still;

--- Advent Of Code/Day 5/Supply Stacks/src/main.rs
+++ Advent Of Code/Day 5/Supply Stacks/src/main.rs
@@ -2,6 +2,15 @@ use std::io::{BufRead,BufReader};
 use std::fs::File;
 use std::env;
 
+fn is_item(item: char) -> bool { 
+    if ((item as u32) <= 90) && ((item as u32) >= 65) { 
+        return true;
+    }
+    else { 
+        return false;
+    }
+}
+
 fn main() {
     let args: Vec<String> = env::args().collect();
     if args[1].is_empty() {
@@ -21,48 +30,38 @@ fn main() {
     let reader = BufReader::new(file);
 
     let results: Vec<_> = reader.lines().collect();
+    let mut contents_of_stacks_as_lists: Vec<Vec<char>> = Vec::new();
+    let mut indices: Vec<usize> = Vec::new();
 
-    let mut stacks: Vec<Vec<u32>> = Vec::new();
+    for i in (0..8).rev() {
+        let line = match &results[i] {
+            Ok(p) => p,
+            Err(_error) => return
+        };
 
-    match &results[8] {
-        Ok(contents) => {
-            let numbers: Vec<i32> = contents
-                .trim() 
-                .split_whitespace() 
-                .map(|s| s.parse::<i32>().unwrap()) 
-                .collect(); 
-            if let Some(number_of_stacks) = numbers.last() {
-
-                for _i in 0..*number_of_stacks { 
-                    let small_stack: Vec<u32> = Vec::new();
-                    stacks.push(small_stack);
-                }
-
-                for i in (0..8).rev() {
-                    match &results[i] {
-                        Ok(contents) => {
-                            println!("{}", contents);
-                        }
-                        Err(_error) => {
-                            println!("Oopsies\n");
-                        }
-                    }
-                }
+        let line_as_list: Vec<char> = line.chars().collect();
 
 
+        if i == 7 { 
+            for i in 0..(line_as_list.len() as usize) { 
+                if is_item(line_as_list[i]){ 
+                    indices.push(i);
+                } 
             }
-            else { 
-                println!("empty vector\n");
-            }
-
         }
-        Err(_error) => {
-            println!("Oooopsies hehe\n");
-            return;
+        contents_of_stacks_as_lists.push(line.chars().collect());
+    }
+
+    let mut stacks: Vec<Vec<char>> = Vec::with_capacity(indices.len());
+
+    for item in contents_of_stacks_as_lists { 
+        for i in 0..indices.len() {
+            if is_item(item[i]) { 
+                stacks[i].push(item[i]);
+
+            }
         }
     }
 
     dbg!(stacks);
-
-
 }
